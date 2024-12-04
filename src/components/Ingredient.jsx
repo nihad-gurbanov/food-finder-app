093492bc0776619dc224361f51fc472f944f62a7
Nihad Gurbanov <hello@nihadgurbanov.com>
2024-12-04 16:05:39 +0400

    ingredient component and styling

---
+++ src/components/Ingredient.jsx
@@ -0,0 +1,24 @@
+import styles from "./ingredient.module.css";
+
+export default function Ingredient({ ingredient }) {
+  return (
+    <div>
+      <div className={styles.ingredientContainer}>
+        <div className={styles.imageContainer}>
+          <img className={styles.image}
+            src={
+              `https://spoonacular.com/cdn/ingredients_100x100/` +
+              ingredient.image
+            }
+          />
+        </div>
+        <div className={styles.nameContainer}>
+          <div className={styles.name}>{ingredient.name}</div>
+          <div className={styles.amount}>
+            {ingredient.amount} {ingredient.unit}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
