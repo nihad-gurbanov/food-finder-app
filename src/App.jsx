a893a1fc34b9be5ef99ad79fbd63337022fc243b
Nihad Gurbanov <hello@nihadgurbanov.com>
2024-12-04 15:12:41 +0400

    App component and style

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import Search from "./components/Search";
+import FoodList from "./components/FoodList";
+import Nav from "./components/Nav";
+import Container from "./components/Container";
+import "./App.css";
+import InnerContainer from "./components/InnerContainer";
+import FoodDetails from "./components/FoodDetails";
+
+function App() {
+  const [foodData, setFoodData] = useState([]);
+  const [foodId, setFoodId] = useState("658615");
+  return (
+    <div className="App">
+      <Nav />
+      <Search foodData={foodData} setFoodData={setFoodData} />
+      <Container>
+        <InnerContainer>
+          <FoodList setFoodId={setFoodId} foodData={foodData} />
+        </InnerContainer>
+        <InnerContainer>
+          <FoodDetails foodId={foodId} />
+        </InnerContainer>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
